7371fb67cf8f36f3cfa1acb21d4eb0475a72404d
sethgonzales <sethgonzales157@gmail.com>
2023-11-17 11:15:20 -0800

    complete bean list, begin bean control

--- src/components/BeanList.js
+++ src/components/BeanList.js
@@ -2,16 +2,27 @@ import React from 'react';
 import PropTypes from "prop-types";
 import Bean from './Bean';
 
-function BeanList(props){
+function BeanList(props) {
   return (
     <React.Fragment>
-
+      {props.beanList.map((bean) =>
+        <Bean
+          whenBeanClicked={props.onBeanSelection}
+          name={bean.name}
+          origin={bean.origin}
+          roast={bean.roast}
+          weight={bean.weight}
+          price={bean.price}
+          id={bean.id}
+          key={bean.id} />
+      )}
     </React.Fragment>
   )
 }
 
 BeanList.PropTypes = {
-
+  beanList: PropTypes.array,
+  onBeanSelection: PropTypes.func
 }
 
 export default BeanList;
