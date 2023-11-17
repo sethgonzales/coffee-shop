572deae7a91b78b33a53640d381ec8342d88c4f0
sethgonzales <sethgonzales157@gmail.com>
2023-11-17 10:36:57 -0800

    begin bean list

---
+++ src/components/BeanList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import Bean from './Bean';
+
+function BeanList(props){
+  return (
+    <React.Fragment>
+
+    </React.Fragment>
+  )
+}
+
+BeanList.PropTypes = {
+
+}
+
+export default BeanList;
